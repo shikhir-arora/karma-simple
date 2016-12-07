05e1402ca826fd9905764178626e81872c56d3a0
Shikhir Arora <shikhir@thesarogroup.com>
2016-12-07 09:18:56 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const TOKEN = '';
+ 
+const Discord = require('discord.js');
+const localStorage = new require('node-localstorage').LocalStorage('cache');
+ 
+const client = new Discord.Client();
+ 
+client.on('message', (message) => {
+  if (message.author.bot) return;
+  let type;
+  if (message.cleanContent.endsWith('--')) {
+    type = 'minus';
+  } else if (message.cleanContent.endsWith('++')) {
+    type = 'plus';
+  } else {
+    return;
+  }
+  const item = message.cleanContent.replace(/(\+\+|--)$/, '').trim().toLowerCase();
+  let count = localStorage.getItem(item) || 0;
+  if (type === 'minus') count--;
+  else if (type === 'plus') count++;
+  console.log(`[KARMA] ${item} ${type}`);
+  localStorage.setItem(item, count);
+  message.channel.sendMessage(`${item} has ${count} karma!`);
+});
+ 
+client.on('ready', () => {
+  console.log(`[READY] ${client.user.username}#${client.user.discriminator} ${client.user.id}`);
+});
+ 
+client.login(TOKEN);
