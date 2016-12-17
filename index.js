c21ec126ed8911cdaa9a1499a69e11432696afb1
Shikhir Arora <shikhir@thesarogroup.com>
2016-12-16 21:34:15 -0500

    Update

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const TOKEN = '';
- 
+const PREFIX = '^';
+
 const Discord = require('discord.js');
 const localStorage = new require('node-localstorage').LocalStorage('cache');
  
 const client = new Discord.Client();
-const PREFIX = '^';
 
 
+client.on('ready', () => {
+  console.log(`[READY] ${client.user.username}#${client.user.discriminator} ${client.user.id}`);
+});
+
+
+ 
 client.on('message', (message) => {
   if (message.author.bot) return;
   let type;
@@ -14,24 +20,23 @@ client.on('message', (message) => {
     type = 'minus';
   } else if (message.cleanContent.endsWith('++')) {
     type = 'plus';
-  } else if (message.content.startsWith('PREFIX' + '${item}') {
-     let value = localStorage.getItem(item);
-     message.channel.sendMessage(`The last I checked ${item} has ${value} karma`);
   } else {
-      return;
+    return;
   }
-
-const item = message.cleanContent.replace(/(\+\+|--)$/, '').trim().toLowerCase();
+  const item = message.cleanContent.replace(/(\+\+|--)$/, '').trim().toLowerCase();
   let count = localStorage.getItem(item) || 0;
   if (type === 'minus') count--;
   else if (type === 'plus') count++;
   console.log(`[KARMA] ${item} ${type}`);
   localStorage.setItem(item, count);
   message.channel.sendMessage(`${item} has ${count} karma!`);
-});
  
-client.on('ready', () => {
-  console.log(`[READY] ${client.user.username}#${client.user.discriminator} ${client.user.id}`);
+  if (text.content.startsWith('PREFIX' + '${item}')) {
+     text.channel.sendMessage(`The last I checked ${item} has ${count} karma..`);
+  }
+ 
 });
  
 client.login(TOKEN);
+
+ 
