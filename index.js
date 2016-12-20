c51d89afbbb55a91e68460a19daf6da03a9ce2d3
Shikhir Arora <shikhir@thesarogroup.com>
2016-12-20 02:53:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
+// CONFIG //
+
 const TOKEN = '';
-const PREFIX = '*';
+const PREFIX = '';
+const EXPLAIN = false;
 
 const Discord = require('discord.js');
 const localStorage = new require('node-localstorage').LocalStorage('cache');
+const Ratelimiter = require('./Ratelimiter.js');
+const rl = new Ratelimiter();
 
 const client = new Discord.Client();
 
+client.on('message', (message) => {
+  if (message.author.bot) return;
+  const check = rl.check(message);
+  if (check === true) {
+    if (message.cleanContent.startsWith(PREFIX)) {
+      const item = message.cleanContent.replace(PREFIX, '').trim();
+      const count = localStorage.getItem(item) || 0;
+      message.reply(`${item} has ${count} karma!`);
+    } else {
+      let type;
+      if (message.cleanContent.endsWith('--')) {
+        type = 'minus';
+      } else if (message.cleanContent.endsWith('++')) {
+        type = 'plus';
+      } else {
+        return;
+      }
+      const item = message.cleanContent.replace(/(\+\+|--)$/, '').trim().toLowerCase();
+      let count = localStorage.getItem(item) || 0;
+      if (type === 'minus') count--;
+      else if (type === 'plus') count++;
+      console.log(`[KARMA] ${item} ${type}`);
+      localStorage.setItem(item, count);
+      message.channel.sendMessage(`${item} has ${count} karma!`);
+    }
+  } else {
+    if (EXPLAIN) message.reply(`Sorry, you have to wait ${check} seconds!`);
+  }
+});
 
 client.on('ready', () => {
   console.log(`[READY] ${client.user.username}#${client.user.discriminator} ${client.user.id}`);
-});
-
-
-
-client.on('message', (message) => {
-
-   string contentmain = message.cleanContent;
-   const texts = localStorage.getItem(contentmain);
-
-  if (message.content.startsWith(texts)) {
-     console.log(`Testing ${texts}`);
-     message.channel.sendMessage(`Testing ${texts}`);
-  }
-
-
-  if (message.author.bot) return;
-  let type;
-  if (message.cleanContent.endsWith('--')) {
-    type = 'minus';
-  } else if (message.cleanContent.endsWith('++')) {
-    type = 'plus';
-  } else {
-    return;
-  }
-  const item = message.cleanContent.replace(/(\+\+|--)$/, '').trim().toLowerCase();
-  let count = localStorage.getItem(item) || 0;
-  if (type === 'minus') count--;
-  else if (type === 'plus') count++;
-  console.log(`[KARMA] ${item} ${type}`);
-  localStorage.setItem(item, count);
-  message.channel.sendMessage(`${item} has ${count} karma!`);
-
-
 });
 
 client.login(TOKEN);
