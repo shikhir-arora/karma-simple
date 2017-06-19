79e594034059d7ea61f4201cb8fb102392337a9c
Shikhir Arora <shikhir@thesarogroup.com>
2017-06-18 22:21:04 -0400

    Auto Install Script - Minor Fix

--- installer.sh
+++ installer.sh
@@ -29,23 +29,32 @@ else
     exit 1
 fi
 
+# Make temporary directory to ensure fresh install. The package will be downloaded and the node_modules will be installed in a temporary directory
+# After installing the modules, karma-simple (which includes the node_modules) will be moved out of the temporary folder and that temporary folder will be deleted
+
+directory=$(pwd)
+tempinstalldir=karmatmp
+
+rm -r "$tempinstalldir" 1>/dev/null 2>&1
+mkdir "$tempinstalldir"
+cd "$tempinstalldir"
 
 echo ""
-echo "Downloading KarmaBot, please wait."
+echo "Downloading KarmaBot, please wait.."
 git clone --recursive --depth 1 https://github.com/shikhir-arora/karma-simple.git || failed "Cannot install. Ensure you have permissions!"
 echo ""
 echo "KarmaBot downloaded!"
 
-
-[[ -d karma-simple ]] || failed "Could not find the karma-simple folder!"
-cd karma-simple || failed "Could not enter the karma-simple folder - please check permissions!"
-
 echo ""
-echo "Downloading KarmaBot dependencies"
-npm install -g || failed "Could not download the karma-simple node packages - please check permissions!"
+echo "Downloading KarmaBot dependencies.."
+cd $directory/$tempinstalldir/karma-simple || failed "Could not enter the karma-simple folder - please check permissions!"
+npm install 
+
+cd "$directory"
+mv "$tempinstalldir"/karma-simple karma-simple
+rm -r "$tempinstalldir"
 
 echo ""
 echo "Installation Complete. Please edit config.json.example and save as config.json"
 echo "To start the bot, use node karma.js or set up pm2/tmux to simplify things later - please see the readme!"
 exit 0
-
