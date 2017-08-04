ace8173437b5b7afbd7c3a9c3e8f2288d0804fc8
Shikhir Arora <shikhir@thesarogroup.com>
2017-08-03 21:55:11 -0400

    Update Ratelimiter

--- Ratelimiter.js
+++ Ratelimiter.js
@@ -1,22 +1,22 @@
 class Ratelimiter {
   constructor () {
-    this.tracked = [];
+    this.tracked = []
 
     setInterval(() => {
       for (const x in this.tracked) {
-        if (this.tracked[x] > 0) this.tracked[x]--;
+        if (this.tracked[x] > 0) this.tracked[x]--
       }
-    }, 2000);
+    }, 3000)
   }
 
   check (message) {
     if (!(message.author.id in this.tracked)) {
-      this.tracked[message.author.id] = 0;
+      this.tracked[message.author.id] = 0
     }
-    this.tracked[message.author.id]++;
-    if (this.tracked[message.author.id] > 1) return this.tracked[message.author.id];
-    return true;
+    this.tracked[message.author.id]++
+    if (this.tracked[message.author.id] > 1) return this.tracked[message.author.id]
+    return true
   }
 }
 
-module.exports = Ratelimiter;
+module.exports = Ratelimiter
