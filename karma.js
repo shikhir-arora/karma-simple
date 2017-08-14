118078f278b6729624976e92b28f56e5ef79d8e6
Shikhir Arora <shikhir@thesarogroup.com>
2017-08-13 23:59:58 -0400

    Beginning of 1.0.5..Blacklist / Cleanups
    1.0.5 part 1
    Blacklist
    Cleanup
    Update packages

--- karma.js
+++ karma.js
@@ -14,6 +14,9 @@ client.on('message', (message) => {
   const check = rl.check(message)
   if (check === true) {
     if (message.cleanContent.startsWith(config.prefix)) {
+      if ((message.guild.roles.find('name', 'NoKarma')) && (message.member.roles.has(message.guild.roles.find('name', 'NoKarma').id))) {
+        return message.reply(`You are not allowed to lookup Karma. Please contact a server mod/admin/staff member.`)
+      }
       const keyword = message.cleanContent.replace(config.prefix, '').trim() // Inputs ARE case sensitive; i.e. "test" and "Test" are different entries. To change to case-insensitive, replace .trim() to .trim().toLowerCase()
       const count = localStorage.getItem(keyword) || 0
       message.reply({
@@ -27,7 +30,10 @@ client.on('message', (message) => {
           timestamp: new Date()
         }
       })
-    } else {
+    } else if ((message.cleanContent.endsWith('--')) || message.cleanContent.endsWith('++')) {
+      if ((message.guild.roles.find('name', 'NoKarma')) && (message.member.roles.has(message.guild.roles.find('name', 'NoKarma').id))) {
+        return message.reply(`You are not allowed to lookup Karma. Please contact a server mod/admin/staff member.`)
+      }
       let type
       if (message.cleanContent.endsWith('--')) {
         type = 'minus'
@@ -60,20 +66,22 @@ client.on('message', (message) => {
 })
 
 client.on('message', (message) => {
-  if (message.content.startsWith('<@255110583072980992>')) {
+  if (message.author.bot) return
+  if (message.content.startsWith(`<@${client.user.id}>` + ` help`)) {
     message.reply({
       embed: new Discord.RichEmbed()
-        .setTitle('KarmaBot Help & Information')
-        .setURL('https://discordbots.org/bot/255110583072980992')
-        .setThumbnail('https://cdn.discordapp.com/avatars/255110583072980992/93a04a6cd5fffa75fb891f5b020f639e.png')
+        .setTitle(`KarmaBot Help & Information`)
+        .setURL(`https://discordbots.org/bot/255110583072980992`)
+        .setThumbnail(message.guild.iconURL)
         .setColor(Math.floor(Math.random() * (0xFFFFFF + 1)))
-        .setDescription('**KarmaBot Help and Information (basic usage, invite URL, support)**')
-        .addField('**❯❯ Add Karma (++):**', 'To **add or increase** karma, type *any* keyword (can be a username, emoji, or any string of text) followed by two plus symbols **++** For example, typing **keyword++** will increase the karma of keyword by one.', false)
-        .addField('**❯❯ Subtract Karma (--):**', 'To **subtract or decrease** karma, type *any* keyword (can be a username, emoji, or any string of text) followed by two minus symbols **--** For example, typing **keyword--** will decrease the karma of keyword by one.', false)
-        .addField('**❯❯ Lookup Karma (>k):**', 'To **lookup** karma, type **>k** followed by the keyword to lookup. For example, typing **>k keyword** will return the karma of keyword. This is shared across all guilds using KarmaBot.', false)
+        .setDescription(`**KarmaBot Help and Information (basic usage, invite URL, support)**`)
+        .addField(`**❯❯ Add Karma (++):**`, `To **add or increase** karma, type *any* keyword (can be a username, emoji, or any string of text) followed by two plus symbols **++** For example, typing **keyword++** will increase the karma of keyword by one.`, true)
+        .addField(`**❯❯ Subtract Karma (--):**`, `To **subtract or decrease** karma, type *any* keyword (can be a username, emoji, or any string of text) followed by two minus symbols **--** For example, typing **keyword--** will decrease the karma of keyword by one.`, true)
+        .addField(`**❯❯ Lookup Karma (>k):**`, `To **lookup** karma, type **>k** followed by the keyword to lookup. For example, typing **>k keyword** will return the karma of keyword. This is shared across all guilds using KarmaBot.`, true)
+        .addField(`**❯❯ Blacklist (Per Guild):**`, `To **blacklist** a user from being able to lookup, add or subtract Karma in a guild, create the role **NoKarma** and assign it to the users you wish to blacklist. By default this bot will take commands from any user, but messages [are internally rate-limited for spam protection](https://cdn.rawgit.com/shikhir-arora/karma-simple/3848016d/Ratelimiter.js).`, true)
         .addBlankField()
         .addField('**❯❯ Invite KarmaBot:**', `**To Invite KarmaBot**, [click here (requires Manage Server permissions)](https://discordapp.com/oauth2/authorize?client_id=255110583072980992&scope=bot&permissions=201673792).`, true)
-        .addField('**❯❯ Support:**', '**For support, visit:** https://discord.io/joinec', true)
+        .addField(`**❯❯ Support:**`, `**For support, visit:** [our Discord server](https://discord.io/joinec) or [GitHub](https://github.com/shikhir-arora/karma-simple/issues).`, true)
         .setTimestamp()
 
     })
@@ -104,7 +112,7 @@ client.on('message', async (message) => {
           .then(res => {
             message.channel.send({
               embed: new Discord.RichEmbed()
-                .setTitle('Eval output exceeds 2000 characters. View Gist')
+                .setTitle('Eval output exceeds 2000 characters. View Gist.')
                 .setURL(`${res.html_url}`)
                 .setColor(Math.floor(Math.random() * (0xFFFFFF + 1)))
                 .setDescription(`Eval output exceeds 2000 characters. View Gist [here](${res.html_url}).`)
@@ -138,7 +146,7 @@ client.on('message', async (message) => {
           .then(res => {
             message.channel.send({
               embed: new Discord.RichEmbed()
-                .setTitle('Console output exceeds 2000 characters. View Gist')
+                .setTitle('Console output exceeds 2000 characters. View Gist.')
                 .setURL(`${res.html_url}`)
                 .setColor(Math.floor(Math.random() * (0xFFFFFF + 1)))
                 .setDescription(`Console output exceeds 2000 characters. View Gist [here](${res.html_url}).`)
@@ -148,7 +156,7 @@ client.on('message', async (message) => {
       } else {
         stdout && message.channel.send(`Info: \n\`\`\`${stdout}\`\`\``)
         stderr && message.channel.send(`Errors: \n\`\`\`${stderr}\`\`\``)
-        if (!stderr && !stdout) { message.react('\uD83E\uDD14') }
+        if (!stderr && !stdout) { message.react('\u2705') }
       }
     })
   }
@@ -195,5 +203,9 @@ client.on('reconnecting', () => {
 
 client.on('error', console.error)
 client.on('warn', console.warn)
+
+process.on('unhandledRejection', (error) => {
+  console.error(`Uncaught Promise Error: \n${error.stack}`)
+})
 
 client.login(config.token)
