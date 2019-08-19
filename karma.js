a4145ac164fcad178000c0fc2022fc1cd6b2b3c2
Shikhir Arora <shikhir@giesela.io>
2019-08-18 23:21:11 -0400

    d.js memory optimisations
    ignore gateway events that we do not care for, reduce stored message cache

--- karma.js
+++ karma.js
@@ -11,10 +11,11 @@ const c = require('ansi-colors')
 const config = require('./config.json')
 const Ratelimiter = require('./Ratelimiter.js')
 const rl = new Ratelimiter()
-const client = new Discord.Client()
 const Enmap = require('enmap')
 const EnmapMongo = require('enmap-mongo')
 require('log-timestamp')(function () { return '[' + new Date().toISOString() + '] %s' })
+const EVENTS_LIST = ['TYPING_START', 'MESSAGE_DELETE', 'MESSAGE_UPDATE', 'PRESENCE_UPDATE', 'VOICE_STATE_UPDATE', 'VOICE_SERVER_UPDATE', 'USER_NOTE_UPDATE', 'CHANNEL_PINS_UPDATE']
+const client = new Discord.Client({ disabledEvents: EVENTS_LIST, messageCacheMaxSize: 100 })
 
 client.karmaStore = new Enmap({ provider: new EnmapMongo({
   name: `karmaStore`,
