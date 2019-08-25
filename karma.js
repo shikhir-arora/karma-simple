d24add1225493356264eb61039a55796651c1276
Shikhir Arora <shikhir@giesela.io>
2019-08-25 01:54:42 -0400

    use randomcolor package
    Signed-off-by: Shikhir Arora <shikhir@giesela.io>

--- karma.js
+++ karma.js
@@ -1,4 +1,4 @@
-if (process.version.slice(1).split('.')[0] < 11) throw new Error(`Node must be v11+ - please upgrade to the latest version of Node!`)
+if (process.version.slice(1).split('.')[0] < 11) throw new Error('Node must be v11+ - please upgrade to the latest version of Node!')
 
 const Discord = require('discord.js')
 const axios = require('axios')
@@ -11,17 +11,19 @@ const c = require('ansi-colors')
 const config = require('./config.json')
 const Ratelimiter = require('./Ratelimiter.js')
 const rl = new Ratelimiter()
+const randomColor = require('randomcolor')
 const Enmap = require('enmap')
 const EnmapMongo = require('enmap-mongo')
 require('log-timestamp')(function () { return '[' + new Date().toISOString() + '] %s' })
 const EVENTS_LIST = ['TYPING_START', 'MESSAGE_DELETE', 'MESSAGE_UPDATE', 'PRESENCE_UPDATE', 'VOICE_STATE_UPDATE', 'VOICE_SERVER_UPDATE', 'USER_NOTE_UPDATE', 'CHANNEL_PINS_UPDATE']
 const client = new Discord.Client({ disabledEvents: EVENTS_LIST, messageCacheMaxSize: 100 })
 
-client.karmaStore = new Enmap({ provider: new EnmapMongo({
-  name: `karmaStore`,
-  dbName: `enmap`,
-  url: ''
-})
+client.karmaStore = new Enmap({
+  provider: new EnmapMongo({
+    name: 'karmaStore',
+    dbName: 'enmap',
+    url: ''
+  })
 })
 
 client.on('message', async (message) => {
@@ -40,14 +42,14 @@ client.on('message', async (message) => {
       try {
         await message.reply({
           embed: {
-            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            color: randomColor(),
             author: {
               name: client.user.username,
-              icon_url: client.user.displayAvatarURL
+              icon_url: client.user.displayAvatarURL()
             },
             description: `${keyword} has **${client.karmaStore.getProp(keyword, 'numKarma') || 0}** Karma!`,
             footer: {
-              text: `KarmaBot by .vlexar#0001`
+              text: 'KarmaBot by .vlexar#0001'
             },
             timestamp: new Date()
           }
@@ -58,7 +60,7 @@ client.on('message', async (message) => {
     } else if ((message.cleanContent.endsWith('--')) || message.cleanContent.endsWith('++')) {
       if (message.channel.type === 'dm') return
       if ((message.guild.roles.find(role => role.name === 'NoKarma')) && (message.member.roles.has(message.guild.roles.find(role => role.name === 'NoKarma').id))) {
-        message.reply(`You are not allowed to add or subtract Karma at this time. Please contact a server mod/admin/staff member. Type \`@KarmaBot help\` for more info.`)
+        message.reply('You are not allowed to add or subtract Karma at this time. Please contact a server mod/admin/staff member. Type `@KarmaBot help` for more info.')
         return message.react('\uD83D\uDD34')
       }
       let type
@@ -84,14 +86,14 @@ client.on('message', async (message) => {
       try {
         await message.channel.send({
           embed: {
-            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+            color: randomColor(),
             author: {
               name: client.user.username,
-              icon_url: client.user.displayAvatarURL
+              icon_url: client.user.displayAvatarURL()
             },
             description: `[KARMA] **${keyword}** has **${client.karmaStore.getProp(keyword, 'numKarma') || 0}** Karma. To lookup later use  **${config.prefix}**  and type **${config.prefix} ${keyword}**`,
             footer: {
-              text: `KarmaBot by .vlexar#0001`
+              text: 'KarmaBot by .vlexar#0001'
             },
             timestamp: new Date()
           }
@@ -103,27 +105,27 @@ client.on('message', async (message) => {
   }
 
   if (message.content.match(new RegExp(`^<@!?${client.user.id}>( |)$`))) {
-    message.reply(`Hi there! Please type \`@KarmaBot help\` for help using this bot or \`@KarmaBot stats\` to get bot statistics.`)
+    message.reply('Hi there! Please type `@KarmaBot help` for help using this bot or `@KarmaBot stats` to get bot statistics.')
     return message.react('\u2705')
   }
-  if ((message.content.startsWith(`<@!${client.user.id}>` + ` help`)) || message.content.startsWith(`<@${client.user.id}>` + ` help`)) {
+  if ((message.content.startsWith(`<@!${client.user.id}>` + ' help')) || message.content.startsWith(`<@${client.user.id}>` + ' help')) {
     if (message.channel.type === 'dm') return
     try {
       const embed = new Discord.MessageEmbed()
-        .setTitle(`KarmaBot Help & Information`)
+        .setTitle('KarmaBot Help & Information')
         .setThumbnail(message.guild.iconURL)
-        .setURL(`https://discord.io/ec`)
-        .setColor(Math.floor(Math.random() * (0xFFFFFF + 1)))
-        .setDescription(`**KarmaBot Help and Information (basic usage, invite URL, support)**`)
-        .addField(`**❯❯ Add Karma (++):**`, `To **add or increase** karma, type *any* keyword (can be a username, emoji, or any string of text) followed by two plus symbols **++** For example, typing **keyword++** will increase the karma of keyword by one.`, true)
-        .addField(`**❯❯ Subtract Karma (--):**`, `To **subtract or decrease** karma, type *any* keyword (can be a username, emoji, or any string of text) followed by two minus symbols **--** For example, typing **keyword--** will decrease the karma of keyword by one.`, true)
-        .addField(`**❯❯ Lookup Karma (>k):**`, `To **lookup** karma, type **>k** followed by the keyword to lookup. For example, typing **>k keyword** will return the karma of keyword. This is shared across all guilds using KarmaBot.`, true)
-        .addField(`**❯❯ Blacklist (Per Guild):**`, `To **blacklist** a user from being able to add or subtract Karma in a guild, create the role **NoKarma** and assign it to the users you wish to blacklist. Users can still lookup Karma, so this can act as a way for admins/mods to, for example, award points to users without the users all being able to add/remove Karma. By default this bot will take commands from any user, but messages [are internally rate-limited for spam protection](https://cdn.rawgit.com/shikhir-arora/karma-simple/3848016d/Ratelimiter.js).`, true)
-        .addField(`**❯❯ Stats:**`, `For **KarmaBot Stats,** type \`@KarmaBot stats\` - fun stuff!`, true)
+        .setURL('https://discord.io/ec')
+        .setColor(randomColor())
+        .setDescription('**KarmaBot Help and Information (basic usage, invite URL, support)**')
+        .addField('**❯❯ Add Karma (++):**', 'To **add or increase** karma, type *any* keyword (can be a username, emoji, or any string of text) followed by two plus symbols **++** For example, typing **keyword++** will increase the karma of keyword by one.', true)
+        .addField('**❯❯ Subtract Karma (--):**', 'To **subtract or decrease** karma, type *any* keyword (can be a username, emoji, or any string of text) followed by two minus symbols **--** For example, typing **keyword--** will decrease the karma of keyword by one.', true)
+        .addField('**❯❯ Lookup Karma (>k):**', 'To **lookup** karma, type **>k** followed by the keyword to lookup. For example, typing **>k keyword** will return the karma of keyword. This is shared across all guilds using KarmaBot.', true)
+        .addField('**❯❯ Blacklist (Per Guild):**', 'To **blacklist** a user from being able to add or subtract Karma in a guild, create the role **NoKarma** and assign it to the users you wish to blacklist. Users can still lookup Karma, so this can act as a way for admins/mods to, for example, award points to users without the users all being able to add/remove Karma. By default this bot will take commands from any user, but messages [are internally rate-limited for spam protection](https://cdn.rawgit.com/shikhir-arora/karma-simple/3848016d/Ratelimiter.js).', true)
+        .addField('**❯❯ Stats:**', 'For **KarmaBot Stats,** type `@KarmaBot stats` - fun stuff!', true)
         .addBlankField()
-        .addField(`**❯❯ Invite KarmaBot:**`, `**To Invite KarmaBot**, [click here (requires Manage Server permissions)](https://bot.discord.io/karmabot).`, true)
-        .addField(`**❯❯ Support:**`, `**For support, visit:** [our Discord server](https://discord.io/ec) or [GitHub](https://github.com/shikhir-arora/karma-simple/issues).`, true)
-        .setFooter(`Project by .vlexar#0001 | KarmaBot Help`)
+        .addField('**❯❯ Invite KarmaBot:**', '**To Invite KarmaBot**, [click here (requires Manage Server permissions)](https://bot.discord.io/karmabot).', true)
+        .addField('**❯❯ Support:**', '**For support, visit:** [our Discord server](https://discord.io/ec) or [GitHub](https://github.com/shikhir-arora/karma-simple/issues).', true)
+        .setFooter('Project by .vlexar#0001 | KarmaBot Help')
         .setTimestamp()
       await message.reply({ embed })
     } catch (e) {
@@ -131,27 +133,27 @@ client.on('message', async (message) => {
     }
   }
 
-  if ((message.content.startsWith(`<@!${client.user.id}>` + ` stats`)) || message.content.startsWith(`<@${client.user.id}>` + ` stats`)) {
+  if ((message.content.startsWith(`<@!${client.user.id}>` + ' stats')) || message.content.startsWith(`<@${client.user.id}>` + ' stats')) {
     try {
       const embed = new Discord.MessageEmbed()
-        .setTitle(`KarmaBot Stats`)
-        .setURL(`https://karmabot.vlexar.pw`)
-        .setColor(Math.floor(Math.random() * (0xFFFFFF + 1)))
-        .setDescription(`**KarmaBot Stats/Info**`)
-        .addField(`**❯❯ Guilds:**`, `${client.guilds.size.toLocaleString()}`, false)
-        .addField(`**❯❯ Users:**`, `${client.users.size.toLocaleString()}`, false)
-        .addField(`**❯❯ Channels:**`, `${client.channels.size.toLocaleString()}`, false)
-        .addField(`**❯❯ Shards:**`, `${client.ws.shards.size}`, false)
-        .addField(`**❯❯ Uptime:**`, moment.duration(process.uptime(), 'seconds').format('dd:hh:mm:ss'), false)
-        .addField(`**❯❯ CPU:**`, `${os.cpus().length}x ${os.cpus()[0].model}`, false)
-        .addField(`**❯❯ Gateway Ping:**`, `${client.ws.ping.toFixed(5)} ms`, false)
-        .addField(`**❯❯ Load Average:**`, `${os.loadavg()[1]}`, false)
-        .addField(`**❯❯ Memory Usage:**`, `${(process.memoryUsage().rss / 1048576).toFixed(2)}MB / ${(os.totalmem() / 1073741824).toFixed(2)}GB`, false)
-        .addField(`**❯❯ System:**`, `${os.type()} - ${os.arch()} ${os.release()}`, false)
-        .addField(`**❯❯ Node Version:**`, process.version, false)
-        .addField(`**❯❯ Discord.js:**`, `v${Discord.version}`, false)
-        .addField(`**❯❯ GitHub:**`, `[GitHub Repo](https://github.com/shikhir-arora/karma-simple).`, true)
-        .setFooter(`Project by .vlexar#0001 | KarmaBot Stats`)
+        .setTitle('KarmaBot Stats')
+        .setURL('https://karmabot.vlexar.pw')
+        .setColor(randomColor())
+        .setDescription('**KarmaBot Stats/Info**')
+        .addField('**❯❯ Guilds:**', `${client.guilds.size.toLocaleString()}`, false)
+        .addField('**❯❯ Users:**', `${client.users.size.toLocaleString()}`, false)
+        .addField('**❯❯ Channels:**', `${client.channels.size.toLocaleString()}`, false)
+        .addField('**❯❯ Shards:**', `${client.ws.shards.size}`, false)
+        .addField('**❯❯ Uptime:**', moment.duration(process.uptime(), 'seconds').format('dd:hh:mm:ss'), false)
+        .addField('**❯❯ CPU:**', `${os.cpus().length}x ${os.cpus()[0].model}`, false)
+        .addField('**❯❯ Gateway Ping:**', `${client.ws.ping.toFixed(5)} ms`, false)
+        .addField('**❯❯ Load Average:**', `${os.loadavg()[1]}`, false)
+        .addField('**❯❯ Memory Usage:**', `${(process.memoryUsage().rss / 1048576).toFixed(2)}MB / ${(os.totalmem() / 1073741824).toFixed(2)}GB`, false)
+        .addField('**❯❯ System:**', `${os.type()} - ${os.arch()} ${os.release()}`, false)
+        .addField('**❯❯ Node Version:**', process.version, false)
+        .addField('**❯❯ Discord.js:**', `v${Discord.version}`, false)
+        .addField('**❯❯ GitHub:**', '[GitHub Repo](https://github.com/shikhir-arora/karma-simple).', true)
+        .setFooter('Project by .vlexar#0001 | KarmaBot Stats')
         .setTimestamp()
       await message.reply({ embed })
     } catch (e) {
@@ -180,11 +182,11 @@ client.on('message', async (message) => {
         await gist(clean(evaled))
           .then(res => {
             const embed = new Discord.MessageEmbed()
-              .setTitle(`Eval output exceeds 2000 characters. View on Gist.`)
+              .setTitle('Eval output exceeds 2000 characters. View on Gist.')
               .setURL(`${res.html.url}`)
-              .setColor(Math.floor(Math.random() * (0xFFFFFF + 1)))
+              .setColor(randomColor())
               .setDescription(`Eval output exceeds 2000 characters. View Gist [here](${res.html_url}).`)
-              .setFooter(`Eval Output`)
+              .setFooter('Eval Output')
               .setTimestamp()
             message.channel.send({ embed }).catch((e) => message.channel.send(e.message))
           })
@@ -210,11 +212,11 @@ client.on('message', async (message) => {
         await gist(`${stdout}\n\n${stderr}`)
           .then(res => {
             const embed = new Discord.MessageEmbed()
-              .setTitle(`Console output exceeds 2000 characters. View on Gist.`)
+              .setTitle('Console output exceeds 2000 characters. View on Gist.')
               .setURL(`${res.html_url}`)
-              .setColor(Math.floor(Math.random() * (0xFFFFFF + 1)))
+              .setColor(randomColor())
               .setDescription(`Console output exceeds 2000 characters. View Gist [here](${res.html_url}).`)
-              .setFooter(`Exec Output`)
+              .setFooter('Exec Output')
               .setTimestamp()
             message.channel.send({ embed }).catch((e) => message.channel.send(e.message))
           })
@@ -267,7 +269,7 @@ async function postDiscordStats () {
 
 client.on('ready', () => {
   console.log(c.bgWhite(`[READY] Connected as ${c.red.bold.underline(client.user.username)}#${c.cyan.bold(client.user.discriminator)} ${c.green.bold(client.user.id)}`))
-  setInterval(() => client.user.setActivity(`@KarmaBot help`, { type: `WATCHING` }), 90000)
+  setInterval(() => client.user.setActivity('@KarmaBot help', { type: 'WATCHING' }), 90000)
 
   postDiscordStats()
 })
@@ -290,7 +292,7 @@ client.on('disconnect', (event) => {
 })
 
 client.on('reconnecting', () => {
-  console.log(c.bgYellow.italic(`[NOTICE] ReconnectAction: Reconnecting to Discord...`))
+  console.log(c.bgYellow.italic('[NOTICE] ReconnectAction: Reconnecting to Discord...'))
 })
 
 client.on('rateLimit', console.log)
@@ -302,7 +304,7 @@ process.on('unhandledRejection', (error) => {
 })
 
 process.on('uncaughtException', (err) => {
-  let errmsg = (err ? err.stack || err : '').toString().replace(new RegExp(`${__dirname}/`, 'g'), './')
+  const errmsg = (err ? err.stack || err : '').toString().replace(new RegExp(`${__dirname}/`, 'g'), './')
   console.error(c.red.bold(errmsg))
 })
 
